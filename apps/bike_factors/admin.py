873e358b994c5af9da8c8f9e72497d8b7dc59714
Alec Barlow <abarlow85@gmail.com>
2016-07-27 14:03:02 -0700

    initial commit

---
+++ apps/bike_factors/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+
+admin.site.register(BikeOption)
+admin.site.register(FeaturesOption)
+admin.site.register(BrandOption)
+admin.site.register(CosmeticOption)
+admin.site.register(FrameOption)
