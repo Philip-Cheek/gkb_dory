da3e00d2a5d445a0fba9f23eab0c9576faad84ee
Alec Barlow <abarlow85@gmail.com>
2016-07-27 15:17:44 -0700

    admin portal

--- apps/bike_factors/admin.py
+++ apps/bike_factors/admin.py
@@ -4,8 +4,27 @@ from .models import *
 # Register your models here.
 
 
-admin.site.register(BikeOption)
-admin.site.register(FeaturesOption)
-admin.site.register(BrandOption)
-admin.site.register(CosmeticOption)
-admin.site.register(FrameOption)
+class BikeOptionAdmin(admin.ModelAdmin):
+	list_display = ['option', 'price_factor']
+
+class FeaturesOptionAdmin(admin.ModelAdmin):
+	list_display = ['option', 'price_factor']
+	
+
+class BrandOptionAdmin(admin.ModelAdmin):
+	list_display = ['option', 'price_factor']
+	
+
+class CosmeticOptionAdmin(admin.ModelAdmin):
+	list_display = ['option', 'price_factor']
+	
+
+class FrameOptionAdmin(admin.ModelAdmin):
+	list_display = ['option', 'price_factor']
+	
+
+admin.site.register(BikeOption, BikeOptionAdmin)
+admin.site.register(FeaturesOption, FeaturesOptionAdmin)
+admin.site.register(BrandOption, BrandOptionAdmin)
+admin.site.register(CosmeticOption, CosmeticOptionAdmin)
+admin.site.register(FrameOption, FrameOptionAdmin)
